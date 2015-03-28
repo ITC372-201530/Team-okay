390cb465f7d5aef1fd62d73081782d527dffc218
EHummerston <magiktrev3@gmail.com>
2015-03-28 18:58:38 +1100

    Initialising Unity Project
    Signed-off-by: EHummerston <magiktrev3@gmail.com>

---
+++ Assets/Scripts/BackgroundText.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundText : MonoBehaviour {
+
+	private string words;
+	public int maxLength;
+	public float active;
+	public int avgLineLength;
+
+	// Use this for initialization
+	void Start () {
+		words = "";
+		guiText.text = words;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		int rng = (int)(Random.value*(maxLength*2/active));
+		if(words.Length >= maxLength) {
+			words = "";
+		}
+		if (!(rng > maxLength*2)) {
+			if (rng < words.Length) {
+				words = words.Substring (0, words.Length - 2);
+			}
+			else {
+				if (rng%avgLineLength == 0) {
+					words+='\n';
+				}
+				else {
+					words += (char)(rng%95+32);
+				}
+			}
+
+		}
+		guiText.text = words;
+	}
+}
---
+++ Assets/Scripts/SpinningSphere.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningSphere : MonoBehaviour {
+
+	public float angle;
+
+	// Use this for initialization
+	void Start () {
+		applyRotation ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		angle += Time.deltaTime*20;
+		if (angle >= 360) {
+			angle-=360;
+		}
+		applyRotation();
+	}
+
+	void applyRotation()
+	{
+		float theta = Mathf.Deg2Rad * angle;
+		
+		float cs = Mathf.Cos(theta);
+		float sn = Mathf.Sin(theta);
+
+		float xN = cs - sn;
+		float yN = sn + cs;
+
+		transform.position = new Vector3 (xN*3, yN*3, 30);
+	}
+}
