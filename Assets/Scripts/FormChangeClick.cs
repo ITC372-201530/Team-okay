ae1bc572ffe7d96b77db6ca34bd8f3c0152981f2
hlyall01 <evil_harry16@hotmail.com>
2015-05-23 19:52:14 +1000

    ALOT has been done, menu, models, animations imported, code changes
    Signed-off-by: hlyall01 <evil_harry16@hotmail.com>

---
+++ Assets/Scripts/FormChangeClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FormChangeClick : MonoBehaviour {
+	public string level;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnMouseDown () {
+		Application.LoadLevel (level);
+	}
+}
